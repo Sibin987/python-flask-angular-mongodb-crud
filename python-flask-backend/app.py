b9fefac2cbc2e38dd72c4133e6b290667ad2d728
Sibin987 <87222439+Sibin987@users.noreply.github.com>
2022-10-02 19:07:51 +0530

    Update app.py

--- python-flask-backend/app.py
+++ python-flask-backend/app.py
@@ -76,11 +76,8 @@ def onedata(id):
         return jsonify(dataDict)
         
     # DELETE a data
-    if request.method == 'DELETE':
-        db['users'].delete_many({'_id': ObjectId(id)})
-        print('\n # Deletion successful # \n')
-        return jsonify({'status': 'Data id: ' + id + ' is deleted!'})
-
+    
+    
     # UPDATE a data by id
     if request.method == 'PUT':
         body = request.json
